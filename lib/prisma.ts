f597ea628cc1958e38395f723b95ff3aa7917a60
mountaga-tall <mountaga.tall@gmail.com>
2026-09-19 22:39:21 +0000

    Set up Prisma client with PostgreSQL adapter
    Initialize Prisma client with PostgreSQL adapter and handle global instance.

---
+++ lib/prisma.ts
@@ -0,0 +1,28 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL n'est pas configurée.");
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
